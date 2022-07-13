df656e00a0526f0d8d9230f176b2dfb050b2d9cd
Aitor <aitor.a98@gmail.com>
2022-07-13 18:03:39 +0200

    Create conts for sequence

--- parser/index.js
+++ parser/index.js
@@ -2,29 +2,7 @@ import parser from "@babel/parser";
 import generate from "@babel/generator";
 import traverse from "@babel/traverse";
 import * as t from "@babel/types";
-
-function visit_node(node) {
-  console.log(node);
-  if (node.type == "VariableDeclaration") {
-    node.declarations.array.forEach((delcaration) => {
-
-    })
-  }
-}
-
-function process_orchestrator_function(parent_node) {
-  console.log(parent_node);
-
-  var block_statement = parent_node.body;
-  if (block_statement.type == "BlockStatement") {
-    for (var i = 0; i < block_statement.body.length; i++) {
-      visit_node(block_statement.body[i]);
-    }
-  }
-}
-
-// var a = t.functionDeclaration(t.identifier("potato"), [t.identifier("arg1")], t.blockStatement([t.emptyStatement()]));
-// console.log(a);
+import { program } from "@babel/types";
 
 const empty_func = `
 function empty_step(context) {
@@ -33,52 +11,107 @@ function empty_step(context) {
 `;
 
 const workflow_code = `
-function* my_workflow_function(context) {
-    const x = yield context.df.callActivity("F1");
-    const y = yield context.df.callActivity("F2", x);
-    const z = yield context.df.callActivity("F3", y);
-    return    yield context.df.callActivity("F4", z);
-}
-`;
+const df = require("durable-functions");
 
-const workflow_code_name = "my_workflow_function";
+module.exports = df.orchestrator(function*(context) {
+  const x = yield context.df.callActivity("F1");
+  const y = yield context.df.callActivity("F2", x);
+  const z = yield context.df.callActivity("F3", y);
+  return yield context.df.callActivity("F4", z);
+});
+`;
 
 const wf_ast = parser.parse(workflow_code, {
   sourceType: "script",
   plugins: ["jsx", "flow"],
 });
 
-const YieldExp = {
-  FunctionDeclaration(path) {
-    process_orchestrator_function(path.node);
+const state = {
+  stack: [],
+  functions: []
+};
+
+var df_handler;
+
+const DurableFunctionVisitor = {
+  VariableDeclaration(path) {
+    console.log('push');
+    state.stack.push(path);
+  },
+  Identifier(path) {
+    console.log('<<<' + path.node.name)
+  },
+  YieldExpression: {
+    exit(path) {
+      path.replaceWith(t.callExpression(path.node.argument.callee, path.node.argument.arguments))
+      var funcBody = [];
+      var returnVars = [];
+      var statement = state.stack.shift();
+      
+      while (statement != undefined) {
+        if (t.isVariableDeclaration(statement.node)) {
+          statement.node.declarations.forEach(dec => {
+            returnVars.push(dec.id.name);
+          })
+        }
+        funcBody.push(statement.node);
+        statement = state.stack.shift();
+      }
+
+      const returnObjectProperties = [];
+      returnVars.forEach(returnVar => {
+        var objProperty = t.objectProperty(t.stringLiteral(returnVar), t.identifier(returnVar));
+        returnObjectProperties.push(objProperty);
+      })
+
+      const ret = t.objectExpression(returnObjectProperties);
+      funcBody.push(t.returnStatement(ret));
+      const prog = t.program([t.functionDeclaration(t.identifier('continuation_' + state.functions.length), [], t.blockStatement(funcBody))]);
+      state.functions.push(prog);
+      console.log('exit');
+    }
   },
 };
 
-traverse.default(wf_ast, MyVisitor);
+const GlobalScriptVisitor = {
+  CallExpression(path) {
+    // console.log(path);
+    if (path.node.callee.name === 'require' && path.node.arguments.pop().value === 'durable-functions') {
+      df_handler = path.parent.id.name;
+      console.log('Durable Functions handler is "' + df_handler + '"')
+      return;
+    }
 
-// const output = generate.default(
-//     wf_ast,
-//   {
-//     /* options */
-//   },
-//   ""
-// );
+    if (t.isMemberExpression(path.node.callee)
+      && t.isIdentifier(path.node.callee.object)
+      && path.node.callee.object.name === df_handler
+      && t.isIdentifier(path.node.callee.property)
+      && path.node.callee.property.name === 'orchestrator') {
+      path.traverse(DurableFunctionVisitor)
+    }
+  }
+}
 
-// console.log(output.code.trim());
+const RemoveYields = {
+  YieldExpression(path) {
+  }
+}
 
-// var wf_func_node = undefined;
+traverse.default(wf_ast, GlobalScriptVisitor);
 
-// for (var i = 0; i < workflow_node.program.body.length; i++) {
-//   var node = workflow_node.program.body[i];
+console.log(df_handler);
 
-//   if (node.type == "ExpressionStatement" && node.expression.type == "AssignmentExpression") {
-//     var expression = node.expression;
-//     if (expression.right.type == "CallExpression" && expression.right.callee.type == "MemberExpression") {
-//       var callee = expression.right.callee;
-//       if (callee.object.name == "df" && callee.property.name == "orchestrator") {
-//         wf_func_node = expression.right.arguments.pop();
-//         break;
-//       }
-//     }
-//   }
-// }
+const output = generate.default(wf_ast, {}, "");
+
+console.log('========================');
+console.log(output.code);
+console.log('========================');
+
+state.functions.forEach(elem => {
+  // console.log(elem)
+  console.log('---')
+  const out = generate.default(elem, {}, "");
+  console.log(out.code);
+});
+
+
